7a636b9d1c3713aa72ae966be9a40a670de6043d
abhishek1908 <abhi2rast@gmail.com>
2019-05-05 15:48:40 +0530

    mongoose part3

--- lesson2/node-mongoose/index.js
+++ lesson2/node-mongoose/index.js
@@ -13,10 +13,23 @@ connect.then((db)=>{
   })
   .then((dish)=>{
     console.log(dish);
-    return Dishes.find({}).exec();
+    return Dishes.findByIdAndUpdate(dish._id,{
+      $set:{description:"Updated test"}
+    },{
+      new:true
+    }).exec();
   })
-  .then((dishes)=>{
-    console.log(dishes);
+  .then((dish)=>{
+    console.log(dish);
+    dish.comments.push({
+      rating:5,
+      comment:'I\'m getting the sinking feeling!!',
+      author:'Ram Gopal'
+    });
+    return dish.save();
+  })  
+  .then((dish)=>{
+    console.log(dish);
     return Dishes.remove({});
   })
   .then(()=>{
