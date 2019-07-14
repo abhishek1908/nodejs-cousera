dff6639436c75278a12a57241ddeda3e7425cd84
Abhishek Rustagi <abhi2rast@gmail.com>
2019-07-15 01:00:00 +0530

    Update keys

--- lesson4/conFusionServer/config.js
+++ lesson4/conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
   'secretKey':'12345-67890-09876-54321',
   'mongoUrl':'mongodb://localhost:27017/conFusion',
   'facebook':{
-    clientId : '393752084574099',
-    clientSecret:'8d1e2c32dda673ead0f22353e445584d'
+    clientId : 'XXXXXXXXXXXXXX',
+    clientSecret:'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
   }
 }
